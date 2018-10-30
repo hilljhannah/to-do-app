ad64a5e09bb6a340c65a10c2bd1b7102663ee089
Hannah Hill <38006750+hilljhannah@users.noreply.github.com>
2018-10-30 12:04:56 -0500

    style changes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+  const toDoList = document.getElementById('toDolist');
 
+// takes a type and a listener which is a function
+// ES6 arrow function == standard anonymous function
   addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
+
     // get the text //
     let title = newToDoText.value;
 
@@ -17,11 +20,18 @@ function onReady() {
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    // create delete
+    let minusBtn = document.createElement('button');
+    minusBtn.innerHTML = '<span>Delete</span>' ;;
+
+
     // set the title
     newLi.textContent = title;
 
     // attach the checkbox to the Li
     newLi.appendChild(checkbox);
+
+    newLi.appendChild(minusBtn);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
@@ -29,7 +39,7 @@ function onReady() {
     // empty the input
     newToDoText.value = '';
   });
-}
+};
 
 window.onload =  function() {
   onReady();
