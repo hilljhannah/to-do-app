d2ff846ac68d3d7f05efb4610a5fdb1de7f0680a
Hannah Hill <38006750+hilljhannah@users.noreply.github.com>
2018-10-30 12:43:40 -0500

    app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ function onReady() {
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDolist');
 
+// takes a type and a listener which is a function
+// ES6 arrow function == standard anonymous function
   addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
     // get the text //
@@ -17,20 +19,26 @@ function onReady() {
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    // create delete
+    let minusBtn = document.createElement('button');
+    minusBtn.innerHTML = '<span>Delete</span>' ;;
+
+
     // set the title
     newLi.textContent = title;
 
     // attach the checkbox to the Li
     newLi.appendChild(checkbox);
 
+    newLi.appendChild(minusBtn);
+
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
     // empty the input
     newToDoText.value = '';
-  });
-}
 
+  });
 window.onload =  function() {
   onReady();
 };
